e936c419f3c2c44c9802e89a4ba55ef5b3cb7b3d
Fran Luque <franluque2@gmail.com>
2022-11-26 13:25:32 -0300

    Made Block Copier Easier to use

--- block_copier.js
+++ block_copier.js
@@ -1,4 +1,5 @@
 var savedNbt = '';
+var savedBlock='';
 function init(event) {
   event.item.setCustomName('Scripted Block Copy Tool')
   event.item.setLore(['Empty','Left click a scripted block to record'])
@@ -9,6 +10,7 @@ if(event.type == 2 && event.target.hasTileEntity() && event.target.getName() == 
   if(!event.player.isSneaking()) {
     if(!savedNbt) { /*comment this to allow overriding recorded data*/
       savedNbt = event.target.getBlockEntityNBT()
+      savedBlock=event.target.getName()
       event.item.setLore(['x:'+event.target.pos.x+' y:'+event.target.pos.y+' z:'+event.target.pos.z,'Shift-left click a scripted block to write recorded script','Toss to clear'])
       event.player.message('Recorded block at x:'+event.target.pos.x+' y:'+event.target.pos.y+' z:'+event.target.pos.z)
       event.item.setCustomName('Scripted Block Copy Tool (Filled)')
@@ -18,13 +20,26 @@ if(event.type == 2 && event.target.hasTileEntity() && event.target.getName() == 
        savedNbt.setInteger('x', event.target.pos.x)
        savedNbt.setInteger('y', event.target.pos.y)
        savedNbt.setInteger('z', event.target.pos.z)
-       event.target.setTileEntityNBT(savedNbt);
-       event.player.message('Block at x:'+event.target.pos.x+' y:'+event.target.pos.y+' z:'+event.target.pos.z+' written successfully')
+       var newtar = event.target.setBlock(savedBlock)
+       newtar.setTileEntityNBT(savedNbt);
+       event.player.message('Block at x:'+newtar.pos.x+' y:'+newtar.pos.y+' z:'+newtar.pos.z+' written successfully')
     }
   }
   }
+  else if(event.type == 2 && event.player.isSneaking())
+  {
+    if(savedNbt) {
+      savedNbt.setInteger('x', event.target.pos.x)
+      savedNbt.setInteger('y', event.target.pos.y)
+      savedNbt.setInteger('z', event.target.pos.z)
+      var newtar = event.target.setBlock(savedBlock)
+       newtar.setTileEntityNBT(savedNbt);
+       event.player.message('Block at x:'+newtar.pos.x+' y:'+newtar.pos.y+' z:'+newtar.pos.z+' written successfully')
+   }
+  }
 }
 function toss(event) {
-  savedNbt = ''
+  savedNbt = '';
+  savedBlock='';
   event.item.setCustomName('Scripted Block Copy Tool (Filled)')
 }
